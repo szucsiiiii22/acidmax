125f0b5f6bbe77eff2b108a194188c3fa9aa7403
guideX <guide_X@live.com>
2016-02-28 21:15:10 -0800

    ddd

--- acidmax/com/acidmax_dll.mrc
+++ acidmax/com/acidmax_dll.mrc
@@ -3,15 +3,64 @@
 ; guide_X@live.com
 ; C:\Windows\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe /codebase C:\dev\team-nexgen\acidmax\acidmax\com\acidmax.dll
 
-alias adll.Init {
-  .comopen adll acidmax.SSH
+alias SSH.Init {
+  comopen adll acidmax.SSH
 }
 
-alias adll.TestMessage {
-  var %result = $com(adll,TestMessage, 1)
-  echo -s 12ssh test message12 » $com(adll).result $1 
+alias SSH.Host {
+  var %result = $com(adll, Host, 1, bstr, $2)
+  echo -s 12ssh Host set to12 » $com(adll).result $1 
 }
 
-alias adll.Unload {
+alias SSH.Port {
+  var %result = $com(adll, Port, 1, bstr, $2)
+  echo -s 12ssh port set to12 » $com(adll).result $1 
+}
+
+alias SSH.User {
+  var %result = $com(adll, User, 1, bstr, $2)
+  echo -s 12ssh user set to12 » $com(adll).result $1 
+}
+
+alias SSH.Password {
+  var %result = $com(adll, Password, 1, bstr, $2)
+  echo -s 12ssh password set to 12 » $com(adll).result $1 
+}
+
+alias SSH.Info {
+  var %result = $com(adll, Info, 1)
+  echo -s 12ssh info 12 » $com(adll).result $1 
+}
+
+
+alias SSH.Connect {
+  echo -s 12ssh connecting
+  var %result = $com(adll, Connect, 1)
+  echo -s 12ssh connect 12 » $com(adll).result $1 
+}
+
+alias SSH.List {
+  var %result = $com(adll, Connect, 1)
+  echo -s 12ssh connect 12 » $com(adll).result $1 
+}
+
+alias SSH.Localhost {
+  var %r1 = $com(adll, Host, 1, bstr, 127.0.0.1) 
+  var %r2 = $com(adll, Port, 1, bstr, 2222) 
+  var %r3 = $com(adll, User, 1, bstr, guideX) 
+  var %r4 = $com(adll, Password, 1, bstr, Betazoid#1)
+  echo -s 12ssh info set 
+}
+
+alias SSH.SetInfo {
+  var %r1 = $com(adll, Host, 1, bstr, $1) 
+  var %r2 = $com(adll, Port, 1, bstr, $2) 
+  var %r3 = $com(adll, User, 1, bstr, $3) 
+  var %r4 = $com(adll, Password, 1, bstr, $4)
+  echo -s 12ssh info set 
+}
+
+
+alias SSH.Unload {
   .comclose adll
 }
--- acidmax_dll/acidmax/SSH.cs
+++ acidmax_dll/acidmax/SSH.cs
@@ -10,65 +10,93 @@ namespace acidmax {
     [Guid("EAA4976A-45C3-4BC5-BC0B-E474F4C3C83F")]
     public interface ISSH {
         /// <summary>
-        /// Last Error
+        /// Remote Path
         /// </summary>
+        /// <param name="localPath"></param>
         /// <returns></returns>
-        [DispId(1)]
-        string LastError();
+        [DispId(14)]
+        string RemotePath(string remotePath);
         /// <summary>
         /// Connected
         /// </summary>
         /// <returns></returns>
         [DispId(2)]
-        bool Connected();
+        string Connected();
         /// <summary>
         /// Set Connect Info
         /// </summary>
-        /// <param name="remoteFtp"></param>
-        /// <param name="remotePort"></param>
-        /// <param name="username"></param>
-        /// <param name="password"></param>
-        [DispId(3)]
-        void SetConnectInfo(string remoteFtp, int remotePort, string username, string password);
-        /// <summary>
         /// Disconnect
         /// </summary>
-        [DispId(4)]
+        [DispId(3)]
         void Disconnect();
         /// <summary>
         /// Connect
         /// </summary>
-        [DispId(5)]
-        void Connect();
+        /// <returns></returns>
+        [DispId(4)]
+        string Connect();
         /// <summary>
         /// List Directory
         /// </summary>
         /// <param name="path"></param>
         /// <returns></returns>
-        [DispId(6)]
-        string ListDirectory(string path);
+        [DispId(5)]
+        string List();
         /// <summary>
         /// Download File
         /// </summary>
-        /// <param name="remotePath"></param>
-        /// <param name="localPath"></param>
         /// <returns></returns>
-        [DispId(7)]
-        bool DownloadFile(string remotePath, string localPath);
+        [DispId(6)]
+        string DownloadFile();
         /// <summary>
         /// Upload File
         /// </summary>
         /// <param name="localPath"></param>
         /// <param name="remotePath"></param>
         /// <returns></returns>
-        [DispId(8)]
-        bool UploadFile(string localPath, string remotePath);
+        [DispId(7)]
+        string UploadFile();
         /// <summary>
-        /// Test Message
+        /// Set Host
         /// </summary>
+        /// <param name="host"></param>
+        /// <returns></returns>
+        [DispId(8)]
+        string Host(string host);
+        /// <summary>
+        /// Port
+        /// </summary>
+        /// <param name="port"></param>
         /// <returns></returns>
         [DispId(9)]
-        string TestMessage();
+        string Port(string port);
+        /// <summary>
+        /// User
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        [DispId(10)]
+        string User(string user);
+        /// <summary>
+        /// Password
+        /// </summary>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        [DispId(11)]
+        string Password(string password);
+        /// <summary>
+        /// Local Path
+        /// </summary>
+        /// <param name="localPath"></param>
+        /// <returns></returns>
+        [DispId(12)]
+        string LocalPath(string localPath);
+        /// <summary>
+        /// Info
+        /// </summary>
+        /// <returns></returns>
+        [DispId(13)]
+        string Info();
     }
     /// <summary>
     /// Program Events
@@ -81,70 +109,123 @@ namespace acidmax {
     /// </summary>
     [Guid("0D53A3E8-E51A-49C7-944E-E72A2064F938"), ClassInterface(ClassInterfaceType.None), ComSourceInterfaces(typeof(IProgramEvents))]
     public class SSH : ISSH {
+        private string _remotePath;
+        /// <summary>
+        /// Host
+        /// </summary>
+        private string _host;
+        /// <summary>
+        /// Port
+        /// </summary>
+        private int _port;
+        /// <summary>
+        /// User
+        /// </summary>
+        private string _user;
+        /// <summary>
+        /// Password
+        /// </summary>
+        private string _password;
         /// <summary>
         /// Connection Info
         /// </summary>
-        private ConnectionInfo connectionInfo;
+        private ConnectionInfo _connectionInfo;
         /// <summary>
         /// Client
         /// </summary>
-        private SftpClient client;
+        private SftpClient _client;
         /// <summary>
-        /// Last Error
+        /// Local Path
         /// </summary>
-        private string _lastError;
+        private string _localPath;
         /// <summary>
-        /// Last Error
+        /// Set Host
         /// </summary>
-        public string LastError() {
-            return _lastError;
+        /// <param name="host"></param>
+        /// <returns></returns>
+        public string Host(string host) {
+            _host = host;
+            return host;
         }
         /// <summary>
-        /// Test Message
+        /// Local Path
+        /// </summary>
+        /// <param name="localPath"></param>
+        /// <returns></returns>
+        public string LocalPath(string localPath) {
+            _localPath = localPath;
+            return localPath;
+        }
+        /// <summary>
+        /// Info
         /// </summary>
         /// <returns></returns>
-        public string TestMessage() {
-            return "Testing,";
+        public string Info() {
+            return _host + ":" + _port + "@" + _user;
+        }
+        /// <summary>
+        /// Port
+        /// </summary>
+        /// <param name="port"></param>
+        /// <returns></returns>
+        public string Port(string port) {
+            var n = 0;
+            if (int.TryParse(port, out n)) {
+                _port = n;
+            }
+            return port;
+        }
+        /// <summary>
+        /// User
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        public string User(string user) {
+            _user = user;
+            return user;
+        }
+        /// <summary>
+        /// Password
+        /// </summary>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public string Password(string password) {
+            _password = password;
+            return password;
         }
         /// <summary>
         /// Connected
         /// </summary>
         /// <returns></returns>
-        public bool Connected() {
-            return client.IsConnected;
-        }
-        /// <summary>
-        /// Set Connect Info
-        /// </summary>
-        /// <param name="remoteFtp"></param>
-        /// <param name="remotePort"></param>
-        /// <param name="username"></param>
-        /// <param name="password"></param>
-        public void SetConnectInfo(string remoteFtp, int remotePort, string username, string password) {
-            if (!client.IsConnected) {
-                connectionInfo = new ConnectionInfo(host: remoteFtp, port: remotePort, username: username, authenticationMethods: new AuthenticationMethod[] { new PasswordAuthenticationMethod(username, password) });
+        public string Connected() {
+            if (_client != null) {
+                return _client.IsConnected.ToString();
+            } else {
+                return "false";
             }
         }
         /// <summary>
         /// Disconnect
         /// </summary>
         public void Disconnect() {
-            client.Disconnect();
+            if (_client != null) {
+                _client.Disconnect();
+            }
         }
         /// <summary>
         /// Connect
         /// </summary>
-        public void Connect() {
+        public string Connect() {
             try {
-                client = new SftpClient(connectionInfo);
-                if (connectionInfo != null) {
-                    client = new SftpClient(connectionInfo);
-                    client.Connect();
-                } else {
-                    _lastError = "Connection Info Not Initialised";
+                _connectionInfo = new ConnectionInfo(host: _host, port: _port, username: _user, authenticationMethods: new AuthenticationMethod[] { new PasswordAuthenticationMethod(_user, _password) });
+                _client = new SftpClient(_connectionInfo);
+                if (_connectionInfo != null) {
+                    _client = new SftpClient(_connectionInfo);
+                    _client.Connect();
                 }
+                return "Connected";
             } catch (Exception ex) {
-                _lastError = ex.Message;
+                return "Error: " + ex.Message;
             }
         }
         /// <summary>
@@ -152,52 +233,54 @@ namespace acidmax {
         /// </summary>
         /// <param name="command"></param>
         /// <returns></returns>
-        public string ListDirectory(string path) {
+        public string List() {
             try {
-                var obj = client.ListDirectory(path);
+                var obj = _client.ListDirectory(_remotePath);
                 var msg = "";
                 foreach (var o in obj) {
                     msg += o.Name + Environment.NewLine;
                 }
                 return msg.Trim();
             } catch (Exception ex) {
-                _lastError = ex.Message;
-                return "";
+                return ex.Message;
             }
         }
         /// <summary>
         /// Download File
         /// </summary>
-        /// <param name="remotePath"></param>
-        /// <param name="localPath"></param>
         /// <returns></returns>
-        public bool DownloadFile(string remotePath, string localPath) {
+        public string DownloadFile() {
             try {
-                using (var stream = System.IO.File.OpenRead(localPath)) {
-                    client.DownloadFile(remotePath, stream);
+                using (var stream = System.IO.File.OpenRead(_localPath)) {
+                    _client.DownloadFile(_remotePath, stream);
                 }
-                return true;
+                return "true";
             } catch (Exception ex) {
-                _lastError = ex.Message;
-                return false;
+                return ex.Message;
             }
         }
         /// <summary>
         /// Upload File
         /// </summary>
-        /// <param name="remotePath"></param>
-        /// <param name="localPath"></param>
         /// <returns></returns>
-        public bool UploadFile(string localPath, string remotePath) {
+        public string UploadFile() {
             try {
-                using (var stream = System.IO.File.OpenRead(localPath)) {
-                    client.UploadFile(stream, remotePath);
+                using (var stream = System.IO.File.OpenRead(_localPath)) {
+                    _client.UploadFile(stream, _remotePath);
                 }
-                return true;
+                return "true";
             } catch (Exception ex) {
-                _lastError = ex.Message;
-                return false;
+                return ex.Message;
             }
+        }
+        /// <summary>
+        /// Remote Path
+        /// </summary>
+        /// <param name="remotePath"></param>
+        /// <returns></returns>
+        public string RemotePath(string remotePath) {
+            _remotePath = remotePath;
+            return remotePath;
         }
     }
 }
